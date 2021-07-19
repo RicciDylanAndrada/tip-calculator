058457d2e44f61fd993c6a0a21d6a28df86fa12b
Dylan Andrada <dylanandrada@Dylans-MacBook-Pro.local>
2021-07-18 23:42:01 -0400

    finished most calculations, and button functionality

--- index.js
+++ index.js
@@ -7,20 +7,26 @@ for(var x = 0; x<length;x++){
 }
 
 
+
+
 function calculate(percentage){
   const billAmount = document.getElementById("bill").value
   const billAmount1 = document.getElementById('bill').value
+  const input = document.getElementById('inputButton').value
+
+
+
   const people = document.getElementById("numOfPeople").value
   let sum =0;
   let perPerson = 0;
   let extra = 0;
+
   switch (percentage) {
 
     case "5%":
      extra = (billAmount * 0.05) / people
     sum = extra + billAmount
     perPerson = (billAmount/people) + extra;
-    perPerson = perPerson.toFixed(2);
 
 
 
@@ -32,7 +38,6 @@ function calculate(percentage){
         extra = (parseFloat(billAmount) * 0.10) / people
       sum = extra + billAmount
       perPerson = (billAmount/people) + extra;
-      perPerson = perPerson.toFixed(2);
 
 
 
@@ -41,7 +46,6 @@ function calculate(percentage){
           extra = (billAmount * 0.15) / people
         sum = extra + billAmount
         perPerson = (billAmount/people) + extra;
-        perPerson = perPerson.toFixed(2);
 
 
 
@@ -51,7 +55,6 @@ function calculate(percentage){
             extra = (billAmount * 0.25) / people
           sum = extra + billAmount
           perPerson = (billAmount/people) + extra;
-          perPerson = perPerson.toFixed(2);
 
 
 
@@ -61,21 +64,29 @@ function calculate(percentage){
              extra = (billAmount * 0.50) / people
             sum = extra + billAmount
             perPerson = (billAmount/people) + extra;
-            perPerson = perPerson.toFixed(2);
 
               break;
 
-              case "Custom":
+              case "Reset":
 
+              document.getElementById("sum").innerHTML = "$0.00"
+              document.getElementById("sumPerson").innerHTML = "$0.00"
                 break;
     default:
 
-
-    console.log("Invalid ");
+    extra = (billAmount * input) / people
+    sum = extra + billAmount
+    perPerson = (billAmount/people) + extra;
+    perPerson = perPerson.toFixed(2);
 
 }
-document.getElementById("sum").innerHTML = extra
-document.getElementById("sumPerson").innerHTML = perPerson
+if (  !isNaN(extra) && !isNaN(perPerson) && isFinite(extra)   && isFinite(perPerson)  ){
+document.getElementById("sum").innerHTML = extra.toFixed(2);
+document.getElementById("sumPerson").innerHTML = perPerson.toFixed(2);
+}
+else{
+
+}
 
 
 }
